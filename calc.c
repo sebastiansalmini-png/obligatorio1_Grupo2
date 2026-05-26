be31ddf851d6ef8c1b48fb72d969204ff569efde
sebastiansalmini-png <sebastian.salmini@estudiantes.utec.edu.uy>
2026-05-26 11:01:27 -0300

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Variables principales
+    int opcion;
+    double a, b, resultado;
+    double memoria = 0.0;
+    double ultimo_resultado = 0.0;
+    int hay_resultado = 0;  // 0 = no hay, 1 = sí hay
+}
