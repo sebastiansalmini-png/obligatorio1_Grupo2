f3307996c5a74b24c97c32b41a3b382542912a9d
Sebastián Salmini <sebastian.salmini@estudiantes.utec.edu.uy>
2026-05-26 18:37:20 -0300

    Inicializa proyecto con calculadora en C

---
+++ obligatorio1_ Grupo2/stats.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int numeros[100];
+    int cantidad = 0;
+    int i, j, temp;
+    int suma = 0;
+    double promedio;
+    int min, max;
+    double mediana;
+    int moda_valor, moda_frecuencia;
+    int duplicados = 0;
+    int frecuencias[100][2];  // [valor][frecuencia]
+    int num_unicos = 0;
+    int encontrado;
+
+}
